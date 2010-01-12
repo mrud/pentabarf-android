6787d387777d8241dd030d116f66906f7c6b18ac
cvandeplas <cvandeplas@d996b95c-40c1-4a4f-a6e9-10525f85c8c0>
2010-01-12 21:45:32 +0000

    todo for christophe
    git-svn-id: https://fosdem-android.svn.sourceforge.net/svnroot/fosdem-android@5 d996b95c-40c1-4a4f-a6e9-10525f85c8c0

--- src/org/fosdem/schedules/RoomListActivity.java
+++ src/org/fosdem/schedules/RoomListActivity.java
@@ -5,13 +5,16 @@ package org.fosdem.schedules;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * @author Christophe Vandeplas <christophe@vandeplas.com>
  *
  */
-public class RoomListActivity extends ListActivity {
+public class RoomListActivity extends ListActivity  {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,9 +26,20 @@ public class RoomListActivity extends ListActivity {
 	
 	public void fillData() {
 		String[] room_a = { "foo", "bar" };
-
+		// fetch rooms of the selected day
+		
 		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, room_a));
-
+		// TODO chri - adapt layout to get right arrow to show there are next actions
 	}
+	
+	@Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String roomName = (String) getListView().getItemAtPosition(position);
+        
+        // TODO load list of Events in Room
+    }
+    
+    
 	
 }
