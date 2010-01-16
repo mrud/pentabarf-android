ae2321c98f9590b6c699804f41cef34838259a05
emichbe <emichbe@d996b95c-40c1-4a4f-a6e9-10525f85c8c0>
2010-01-16 19:34:22 +0000

    git-svn-id: https://fosdem-android.svn.sourceforge.net/svnroot/fosdem-android@14 d996b95c-40c1-4a4f-a6e9-10525f85c8c0

---
+++ src/org/fosdem/preferences/Preferences.java
@@ -0,0 +1,6 @@
+package org.fosdem.preferences;
+
+public class Preferences {
+	//2010 schedule: http://fosdem.org/2010/schedule/xml
+	public static String SCHEDULE_URL = "http://archive.fosdem.org/2009/schedule/xml";
+}
