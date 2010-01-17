e8acb1ddd4f489bd2e962d8fda0b4450b0ba91ae
emichbe <emichbe@d996b95c-40c1-4a4f-a6e9-10525f85c8c0>
2010-01-17 02:00:13 +0000

    git-svn-id: https://fosdem-android.svn.sourceforge.net/svnroot/fosdem-android@19 d996b95c-40c1-4a4f-a6e9-10525f85c8c0

--- src/org/fosdem/util/DateUtil.java
+++ src/org/fosdem/util/DateUtil.java
@@ -5,13 +5,18 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import android.util.Log;
+
 public class DateUtil {
-	public static Date getDate(String date) throws ParseException{
+	public static Date getDate(String date) throws ParseException {
 		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		return formatter.parse(date);
-	} 
-	
-	public static int convertStringToMinutes(String minutes){
-		return Integer.parseInt(minutes.substring(0,1))*60+Integer.parseInt(minutes.substring(3,4));
+		Date d = formatter.parse(date);
+		Log.v(DateUtil.class.getName(), d.toString());
+		return d;
+	}
+
+	public static int convertStringToMinutes(String minutes) {
+		return Integer.parseInt(minutes.substring(0, 1)) * 60
+				+ Integer.parseInt(minutes.substring(3, 4));
 	}
 }
