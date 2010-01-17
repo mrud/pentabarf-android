17162442543909f499be3e8659f0d6ab295229c4
emichbe <emichbe@d996b95c-40c1-4a4f-a6e9-10525f85c8c0>
2010-01-17 12:27:57 +0000

    Utility class to get files and images from remote source, with caching possibility.
    git-svn-id: https://fosdem-android.svn.sourceforge.net/svnroot/fosdem-android@23 d996b95c-40c1-4a4f-a6e9-10525f85c8c0

---
+++ src/org/fosdem/util/FileUtil.java
@@ -0,0 +1,106 @@
+package org.fosdem.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+public class FileUtil {
+	public static final String CACHELOCATION = "/data/data/org.fosdem/files/";
+
+	public static Object fetch(String address) throws MalformedURLException,
+			IOException {
+		URL url = new URL(address);
+		Object content = url.getContent();
+		return content;
+	}
+
+	public static Object fetchCached(String address)
+			throws MalformedURLException, IOException {
+		return fetchCached(address, -1);
+	}
+
+	public static Object fetchCached(String address, int hours)
+			throws MalformedURLException, IOException {
+		String cacheName = md5(address);
+		checkAndCreateDirectoryIfNeeded();
+
+		File r = new File(CACHELOCATION + cacheName);
+
+		Date d = new Date();
+		long limit = d.getTime() - (1000 * 60 * 60 * hours);
+
+		if (!r.exists() || (hours != -1 && r.lastModified() < limit)) {
+			InputStream is = (InputStream) fetch(address);
+
+			FileOutputStream fos = new FileOutputStream(CACHELOCATION
+					+ cacheName);
+			int nextChar;
+			while ((nextChar = is.read()) != -1)
+				fos.write((char) nextChar);
+
+			fos.flush();
+		}
+
+		FileInputStream fis = new FileInputStream(CACHELOCATION + cacheName);
+		return fis;
+	}
+
+	public static Drawable fetchCachedDrawable(String url)
+			throws MalformedURLException, IOException {
+		String cacheName = md5(url);
+		InputStream is = (InputStream) fetch(url);
+
+		checkAndCreateDirectoryIfNeeded();
+
+		FileOutputStream fos = new FileOutputStream(CACHELOCATION + cacheName);
+		int nextChar;
+		while ((nextChar = is.read()) != -1)
+			fos.write((char) nextChar);
+
+		fos.flush();
+
+		FileInputStream fis = new FileInputStream(CACHELOCATION + cacheName);
+
+		Drawable d = Drawable.createFromStream(fis, "src");
+
+		return d;
+	}
+
+	public static void checkAndCreateDirectoryIfNeeded() {
+		File f = new File(CACHELOCATION);
+		if (!f.exists())
+			f.mkdir();
+	}
+
+	public static String md5(String str) {
+		try {
+			// Create MD5 Hash
+			MessageDigest digest = java.security.MessageDigest
+					.getInstance("MD5");
+			digest.update(str.getBytes());
+			byte messageDigest[] = digest.digest();
+
+			// Create Hex String
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < messageDigest.length; i++)
+				hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+			String md5 = hexString.toString();
+			Log.v(FileUtil.class.getName(), md5);
+			return md5;
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+}
