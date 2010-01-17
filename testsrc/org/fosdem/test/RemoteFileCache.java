f240c18974090c3d889a75d900b4cd9177c49f09
emichbe <emichbe@d996b95c-40c1-4a4f-a6e9-10525f85c8c0>
2010-01-17 12:44:24 +0000

    git-svn-id: https://fosdem-android.svn.sourceforge.net/svnroot/fosdem-android@25 d996b95c-40c1-4a4f-a6e9-10525f85c8c0

---
+++ testsrc/org/fosdem/test/RemoteFileCache.java
@@ -0,0 +1,23 @@
+package org.fosdem.test;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.fosdem.util.FileUtil;
+
+import android.test.AndroidTestCase;
+
+public class RemoteFileCache extends AndroidTestCase {
+	public void testRemoteFetch(){
+		try {
+			FileUtil.fetchCached("http://fosdem.org/2010/map/room/aw1105/small");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("IOException during remote fetch");
+		}
+	}
+}
