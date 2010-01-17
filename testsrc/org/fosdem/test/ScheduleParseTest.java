9c8ef99ae0b8a1ca95556f1aa04d51baff50cc6c
emichbe <emichbe@d996b95c-40c1-4a4f-a6e9-10525f85c8c0>
2010-01-17 02:01:09 +0000

    git-svn-id: https://fosdem-android.svn.sourceforge.net/svnroot/fosdem-android@20 d996b95c-40c1-4a4f-a6e9-10525f85c8c0

--- testsrc/org/fosdem/test/ScheduleParseTest.java
+++ testsrc/org/fosdem/test/ScheduleParseTest.java
@@ -2,6 +2,7 @@ package org.fosdem.test;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import org.fosdem.db.DBAdapter;
@@ -15,10 +16,10 @@ import org.fosdem.pojo.Schedule;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
-public class ScheduleParseTest extends AndroidTestCase{
+public class ScheduleParseTest extends AndroidTestCase {
 	public static Schedule schedule;
-	
-	public void testScheduleParses(){
+
+	public void testScheduleParses() {
 		ScheduleParser sp = null;
 		try {
 			sp = new ScheduleParser("http://fosdem.org/2010/schedule/xml");
@@ -26,66 +27,81 @@ public class ScheduleParseTest extends AndroidTestCase{
 			e.printStackTrace();
 			fail("Failed to parse from URL");
 		}
-		assertTrue(sp!=null);
+		assertTrue(sp != null);
 		Schedule s = null;
-		
+
 		try {
-			s=sp.parse();
+			s = sp.parse();
 		} catch (ParserException e) {
 			e.printStackTrace();
 			fail("Failed to parse");
 		}
-		assertTrue(s!=null);
-		assertTrue(s.getDays()!=null);
-		assertTrue(s.getDays().size()==2);
+		assertTrue(s != null);
+		assertTrue(s.getDays() != null);
+		assertTrue(s.getDays().size() == 2);
 		assertTrue(s.getConference().getCity().equals("Brussels"));
-		assertTrue(((Day)(s.getDays().toArray()[0])).getRooms().size()>0);
-		Collection<Room> rooms = ((Day)(s.getDays().toArray()[0])).getRooms();
-		assertTrue(((Room)(rooms.toArray()[0])).getEvents().size()>0);
-		schedule=s;
+		assertTrue(((Day) (s.getDays().toArray()[0])).getRooms().size() > 0);
+		Collection<Room> rooms = ((Day) (s.getDays().toArray()[0])).getRooms();
+		assertTrue(((Room) (rooms.toArray()[0])).getEvents().size() > 0);
+		schedule = s;
 	}
-	
-	public void testSchedulePersistence(){
+
+	public void testSchedulePersistence() {
 		DBAdapter db = new DBAdapter(getContext());
+
 		db.open();
-		db.clearEvents();
-		assertTrue(db.getEvents().size()==0);
-		
-		db.persistSchedule(schedule);
-		int total=0;
-		for(Day day:schedule.getDays()){
-			for(Room room:day.getRooms()){
-				for(@SuppressWarnings("unused") Event event:room.getEvents()){
-					total++;
+		try {
+			db.clearEvents();
+			assertTrue(db.getEvents().size() == 0);
+
+			db.persistSchedule(schedule);
+			int total = 0;
+			for (Day day : schedule.getDays()) {
+				for (Room room : day.getRooms()) {
+					total += room.getEvents().size();
 				}
 			}
+			List<Event> events = db.getEvents();
+			Log.v(getClass().getName(), "Event count : " + events.size()
+					+ ", normal total:" + total);
+			assertTrue(total == events.size());
+		} catch (Exception e) {
+			fail(e.getMessage());
+		} finally {
+			db.close();
 		}
-		List<Event> events = db.getEvents();
-		assertTrue(total==events.size());
-		
-		db.close();
 	}
-	
-	public void testScheduleQueryByCriteria(){
+
+	public void testScheduleQueryByCriteria() {
 		DBAdapter db = new DBAdapter(getContext());
 		db.open();
-		List<Event> events = db.getEventsFiltered(null, null, new String[]{"Database"}, null, null, null,null);
-		Log.v(getClass().getName(),"Number of filtered events: "+events.size());
-		//assertTrue(events.size()==3);
-		events=db.getEventsFiltered(null, null,null, null, null, null,new String[]{"English"});
-		assertTrue(events.size()==db.getEvents().size());
+		try {
+			List<Event> events = db.getEventsFiltered(null, null,
+					new String[] { "Database" }, null, null, null, null);
+			Log.v(getClass().getName(), "Number of filtered events: "
+					+ events.size());
+			// assertTrue(events.size()==3);
+			events = db.getEventsFiltered(null, null, null, null, null, null,
+					new String[] { "English" });
+			assertTrue(events.size() == db.getEvents().size());
+		} catch (Exception e) {
+			fail(e.getMessage());
+		} finally {
+			db.close();
+		}
+
 	}
-	
-	public void testScheduleQueryByRoomByTrack(){
+
+	public void testScheduleQueryByRoomByTrack() {
 		DBAdapter db = new DBAdapter(getContext());
 		db.open();
-		
+
 		String[] rooms = db.getRooms();
 		String[] tracks = db.getTracks();
-		
-		assertTrue(rooms.length>0);
-		assertTrue(tracks.length>0);
-		
+
+		assertTrue(rooms.length > 0);
+		assertTrue(tracks.length > 0);
+
 		db.close();
 	}
 }
