7660da5c03136e181f621969aa2fafe158650d44
emichbe <emichbe@d996b95c-40c1-4a4f-a6e9-10525f85c8c0>
2010-01-18 23:38:46 +0000

    git-svn-id: https://fosdem-android.svn.sourceforge.net/svnroot/fosdem-android@54 d996b95c-40c1-4a4f-a6e9-10525f85c8c0

--- src/org/fosdem/util/EventAdapter.java
+++ src/org/fosdem/util/EventAdapter.java
@@ -9,7 +9,6 @@ import org.fosdem.R;
 import org.fosdem.pojo.Event;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
