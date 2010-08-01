6ba6c14e33b714837c3f72ade03aebd347102553
Ulrich Dangel <uli@spamt.net>
2010-08-01 18:45:03 +0200

    Remove unnecessary file

--- src/org/fosdem/preferences/Preferences.java
+++
@@ -1,6 +0,0 @@
-package org.fosdem.preferences;
-
-public class Preferences {
-	//2010 schedule: http://fosdem.org/2010/schedule/xml
-	public static String SCHEDULE_URL = "http://archive.fosdem.org/2009/schedule/xml";
-}
