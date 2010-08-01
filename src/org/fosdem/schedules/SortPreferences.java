afea04562b590bbcc2e6572655aa14d706807c15
Ulrich Dangel <uli@spamt.net>
2010-08-01 19:01:01 +0200

    Add Sort preferences for TrackListActivity

---
+++ src/org/fosdem/schedules/SortPreferences.java
@@ -0,0 +1,37 @@
+package org.fosdem.schedules;
+
+import org.fosdem.R;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+public class SortPreferences extends PreferenceActivity implements
+		OnPreferenceChangeListener {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setTitle(R.string.menu_settings);
+		addPreferencesFromResource(R.xml.sort_preferences);
+		Preference sortPref = (Preference) findPreference(org.fosdem.schedules.Preferences.PREF_SORT);
+		sortPref.setOnPreferenceChangeListener(this);
+
+	}
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		SharedPreferences sharedPreferences = getSharedPreferences(Main.PREFS,
+				Activity.MODE_PRIVATE);
+		Editor edit = sharedPreferences.edit();
+		
+		if (preference.getKey().equals(org.fosdem.schedules.Preferences.PREF_SORT)) {
+			edit.putString(preference.getKey(), (String) newValue);
+		}
+		edit.commit();
+		return true;
+	}
+
+}
