a0eff5c9970e9330d8d62bfc5a3293d8821744fa
Ulrich Dangel <uli@spamt.net>
2010-08-02 16:13:50 +0200

    Show better title for room overview

--- src/org/fosdem/schedules/TrackListActivity.java
+++ src/org/fosdem/schedules/TrackListActivity.java
@@ -89,6 +89,7 @@ public class TrackListActivity extends ListActivity  {
 		return true;
 	}
 	protected void handleRoom() {
+		setTitle("Rooms for Day " + dayIndex);
 		rooms = getRooms();
 		setListAdapter(new RoomAdapter(this, R.layout.track_list, getRooms()));
 	}
